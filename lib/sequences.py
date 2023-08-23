6ca0275a70a66fc5e759082d495a56e5fd62b242
Brian Kibet <kibetsbrian@gmail.com>
2023-08-23 22:39:47 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+
+    if length < 2:
+        print(sequence[:length])
+        return
+    
+    while len(sequence) < length:
+        next_num = sequence[-1] + sequence[-2]
+        sequence.append(next_num)
+
+    print(sequence)
